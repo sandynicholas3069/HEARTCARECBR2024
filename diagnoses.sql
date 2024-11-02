8e7ca772d79b9c580d7e62c7e493f3685b3cbae5
sandynicholas <sandynicholas1201@gmail.com>
2024-11-02 15:01:14 +0700

    Setup Project

---
+++ diagnoses.sql
@@ -0,0 +1,17 @@
+CREATE TABLE diagnoses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    age INT,
+    sex TINYINT,
+    cp TINYINT,
+    trestbps INT,
+    chol INT,
+    fbs TINYINT,
+    restecg TINYINT,
+    thalach INT,
+    exang TINYINT,
+    oldpeak FLOAT,
+    slope TINYINT,
+    ca TINYINT,
+    thal TINYINT,
+    result TINYINT
+);
